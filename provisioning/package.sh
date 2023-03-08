f2f943b39b6ae6dd0423876472ad852ee97fce6b
Vera Kahn <verakahn@nypl.org>
2023-03-08 12:57:40 -0500

    update package.sh

--- provisioning/package.sh
+++ provisioning/package.sh
@@ -1,6 +1,12 @@
 # Build dependencies:
 pip install -r requirements.txt --target ./build
-pip install "psycopg[binary,pool]"
+pip install \
+    --platform manylinux2014_x86_64 \
+    --target=./build \
+    --implementation cp \
+    --python 3.9 \
+    --only-binary=:all: --upgrade \
+    'psycopg[binary,pool]'
 
 # Move required application files into build:
 cp *.py build/.
